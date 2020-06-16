e95802609a12db493e81f0253832493dc9f59a09
Mateo <mateo.betancur@zemoga.com>
2020-06-16 11:32:09 -0500

    menu and main panel

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="contact-form" class="contact-form">
+    <h1 class="contact-form_title">Contact</h1>
+    <div class="separator"></div>
+    <form class="form" @submit="onSubmit">
+      <input
+        required
+        name="name"
+        v-model="contact.name"
+        placeholder="Full Name"
+        type="text"
+        autocomplete="off"
+      />
+      <input
+        required
+        name="email"
+        v-model="contact.email"
+        placeholder="Email Address"
+        type="email"
+        autocomplete="off"
+      />
+      <input
+        required
+        name="email"
+        v-model="contact.subject"
+        placeholder="Subject"
+        type="text"
+        autocomplete="off"
+      />
+      <textarea name="message" v-model="contact.message" rows="4" placeholder="Message"></textarea>
+      <button class="button">Send</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContactForm",
+  data() {
+    return {
+      contact: {
+        name: "",
+        subject: "",
+        email: "",
+        message: ""
+      }
+    };
+  },
+  methods: {
+    clearForm() {
+      for (let field in this.contact) {
+        this.contact[field] = "";
+      }
+    },
+    onSubmit(evt) {
+      evt.preventDefault();
+    //   this.sendEmail();
+    },
+    sendEmail() {
+      Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "slash-252@hotmail.com",
+        Password: "67435E3FA315112018493A3198A6DDB28980",
+        To: "mikebaia@yopmail.com",
+        From: "slash-252@hotmail.com",
+        Subject: "This is the subject",
+        Body: "And this is the body"
+      }).then(message => alert(message));
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,35 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="panel-header">
+    <div class="panel-header_my-photo">
+      <img src="../assets/logo.png" alt="Foto panel principal" />
+    </div>
+    <div class="panel-header_intro">
+      <h1>Miguel en la ciudad</h1>
+      Praesent sed aliquam arcu, non accumsan neque. In odio ante, vulputate ac magna vel, pharetra lobortis quam. Duis enim tortor, egestas et felis id, lobortis malesuada magna. Nunc sit amet sagittis nisi, eu semper nisl. Cras ut dictum nisl. Donec tincidunt eget orci.
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.panel-header {
+  display: flex;
+
+  &_my-photo img {
+    min-height: 550px;
+  }
+  &_intro {
+    padding: 70px 15%;
+    background-color: red; //#fcfcfc;
+  }
 }
 </style>
