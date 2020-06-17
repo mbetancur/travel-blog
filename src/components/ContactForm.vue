9e8f06f96dac5579758f1b817134bbc49a7d0f2f
Mateo <mateo.betancur@zemoga.com>
2020-06-17 11:36:58 -0500

    panels created contact and landing page

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -2,32 +2,41 @@
   <div id="contact-form" class="contact-form">
     <h1 class="contact-form_title">Contact</h1>
     <div class="separator"></div>
+    <div class="contact-cards">
+      <div class="contact-card">(+57) 300 000 0000</div>
+      <div class="contact-card">Colombia - Antioquia</div>
+      <div class="contact-card">travelblog@correo.com</div>
+    </div>
     <form class="form" @submit="onSubmit">
-      <input
-        required
-        name="name"
-        v-model="contact.name"
-        placeholder="Full Name"
-        type="text"
-        autocomplete="off"
-      />
-      <input
-        required
-        name="email"
-        v-model="contact.email"
-        placeholder="Email Address"
-        type="email"
-        autocomplete="off"
-      />
-      <input
-        required
-        name="email"
-        v-model="contact.subject"
-        placeholder="Subject"
-        type="text"
-        autocomplete="off"
-      />
-      <textarea name="message" v-model="contact.message" rows="4" placeholder="Message"></textarea>
+      <div class="form-elements">
+        <div class="inputs">
+          <input
+            required
+            name="name"
+            v-model="contact.name"
+            placeholder="Full Name"
+            type="text"
+            autocomplete="off"
+          />
+          <input
+            required
+            name="email"
+            v-model="contact.email"
+            placeholder="Email Address"
+            type="email"
+            autocomplete="off"
+          />
+          <input
+            required
+            name="email"
+            v-model="contact.subject"
+            placeholder="Subject"
+            type="text"
+            autocomplete="off"
+          />
+        </div>
+        <textarea name="message" v-model="contact.message" rows="4" placeholder="Message"></textarea>
+      </div>
       <button class="button">Send</button>
     </form>
   </div>
@@ -54,14 +63,16 @@ export default {
     },
     onSubmit(evt) {
       evt.preventDefault();
-      this.sendEmail();
+      console.log("email");
+      // this.sendEmail();
     },
     sendEmail() {
       Email.send({
         Host: "smtp.elasticemail.com",
         Username: "slash-252@hotmail.com",
         Password: "67435E3FA315112018493A3198A6DDB28980",
-        To: "mikebaia@yopmail.com",
+        // To: "mikebaia@yopmail.com",
+        To: "mateobetancurgrisales@gmail.com",
         From: "slash-252@hotmail.com",
         Subject: "This is the subject",
         Body: "And this is the body"
@@ -72,4 +83,43 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.contact-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .contact-cards {
+    padding: 100px 0;
+    margin: 100px 0;
+    width: 70%;
+    display: flex;
+    justify-content: space-around;
+    .contact-card {
+      // padding: 30px 45px;
+      padding: 3vh 2vw;
+      border: 1px solid #eee;
+      text-align: center;
+    }
+  }
+
+  .form {
+    display: flex;
+    width: 60%;
+    flex-direction: column;
+    align-items: center;
+    .form-elements {
+      display: flex;
+      flex-direction: row;
+      .inputs {
+        display: flex;
+        flex-direction: column;
+        input {
+          margin: 0 12px 12px;
+          width: 204px;
+          height: 42px;
+        }
+      }
+    }
+  }
+}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="panel-header">
-    <div class="panel-header_my-photo">
-      <img src="../assets/logo.png" alt="Foto panel principal" />
+  <div class="landing-page">
+    <div class="panel-header">
+      <div class="panel-header_my-photo">
+        <img src="../assets/logo.png" alt="Foto panel principal" />
+      </div>
+      <div class="panel-header_intro-text">
+        <h1>Miguel en la ciudad</h1>Praesent sed aliquam arcu, non accumsan neque. In odio ante, vulputate ac magna vel, pharetra lobortis quam. Duis enim tortor, egestas et felis id, lobortis malesuada magna. Nunc sit amet sagittis nisi, eu semper nisl. Cras ut dictum nisl. Donec tincidunt eget orci.
+      </div>
     </div>
-    <div class="panel-header_intro">
-      <h1>Miguel en la ciudad</h1>
-      Praesent sed aliquam arcu, non accumsan neque. In odio ante, vulputate ac magna vel, pharetra lobortis quam. Duis enim tortor, egestas et felis id, lobortis malesuada magna. Nunc sit amet sagittis nisi, eu semper nisl. Cras ut dictum nisl. Donec tincidunt eget orci.
+    <div class="panel-info">
+      <p>soy Miguel. Un joven con grandes aspiraciones en la vida, una de ellas es conocer los lugares fántasticos que posee el planeta, pero primero empezar por mi sector, el Valle de Aburra el cual tiene también ciertos atractivos que te podrían interesar, aquí te dejo algunos lugares de turismo natural, únicos y agradables para pasar un buen rato.</p>
+      <p>Haribus, ommolup tatiscimus ut faceperatur alicidior sum exeri as aliqui apienis net veliquasped milibusci cuptatem estium nobit fuga. Itatur, quo volupti undebit asperspe dolo moluptatio. Icatempores aruptiosam faccata at a ipsum quidi ant omnimi, vit volutemquo explab is sam, eat provide ratatiunt. Ad quam re dolorerferro blaborrum repti as que ernamuscid etur, vernam fugite non nulpario el ipidunt, ommolum harum que voluptio et idelend.</p>
     </div>
-
   </div>
 </template>
 
@@ -23,13 +27,23 @@ export default {
 <style scoped lang="scss">
 .panel-header {
   display: flex;
+  justify-content: space-around;
 
   &_my-photo img {
     min-height: 550px;
   }
-  &_intro {
+  &_intro-text {
     padding: 70px 15%;
     background-color: red; //#fcfcfc;
+  }
+}
+.panel-info {
+  padding: 50px 70px 70px;
+  display: flex;
+  justify-content: space-around;
+
+  p {
+    width: 460px;
   }
 }
 </style>
